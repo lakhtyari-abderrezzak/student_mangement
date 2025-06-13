5ad51077ae6ab23b2d7ebe6e9f590e12664c5c2f
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-13 02:29:14 +0100

    Update autoload.php to include necessary files for application functionality

--- private/core/autoload.php
+++ private/core/autoload.php
@@ -1,3 +1,6 @@
 <?php 
 
+require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/app.php';
+require_once __DIR__ . '/database.php';
+require_once dirname(__DIR__, 1) . '/controllers/Controller.php';
