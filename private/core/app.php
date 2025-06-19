3c13e234b5e5c94f7308563ec9e4ab33d6c36ca2
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-19 15:57:37 +0100

    Refactor URL handling and controller method invocation in App class

--- private/core/app.php
+++ private/core/app.php
@@ -12,18 +12,29 @@ class App
     public function __construct()
     {
         $URL = $this->getURL();
-
+        
         if (file_exists('../private/controllers/' . $URL[0] . '.php')) {
-            $this->controller = $URL[0];
+            $this->controller = strtolower($URL[0]);
             unset($URL[0]);
         }
         require_once '../private/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
+
+        if(isset($URL[1])){
+            if(method_exists($this->controller, $URL[1])){
+                $this->method = strtolower($URL[1]);
+                unset($URL[1]);
+            }
+        }
+        $URL = array_values($URL);
+        $this->params = $URL;
+        call_user_func_array([$this->controller, $this->method, ], $this->params);
+        
     }
 
     private function getURL()
     {
         $url = isset($_GET['url']) ? $_GET['url'] : 'home';
-        return explode('/', filter_var(trim($url, 'home'), FILTER_SANITIZE_URL));
+        return explode('/', filter_var(trim($url, '/'), FILTER_SANITIZE_URL));
     }
 }
