cac2aa9dc5da3047e085778f2a837b712adc9aaa
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-19 15:58:06 +0100

    Update autoload.php to require Controller.php from the current directory

--- private/core/autoload.php
+++ private/core/autoload.php
@@ -3,4 +3,4 @@
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/app.php';
 require_once __DIR__ . '/database.php';
-require_once dirname(__DIR__, 1) . '/controllers/Controller.php';
+require_once __DIR__ . '/Controller.php';
