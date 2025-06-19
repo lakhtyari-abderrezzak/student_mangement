c3c45ade94a2d5ce519091b2f122db67ec41df99
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-19 11:52:01 +0100

    remove: __construct function from student.php

--- private/controllers/Student.php
+++ private/controllers/Student.php
@@ -3,10 +3,6 @@
 
 
 class Student extends Controller{
-    public function __construct(){
-        echo "Welcome to the Student Controller!";
-    }
-
     public function index(){
         echo "This is the index method of the Student Controller.";
     }
