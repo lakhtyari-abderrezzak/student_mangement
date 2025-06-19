b5bba2eb65f9bd0f8324cd1faaa9a51ecbda8f33
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-19 16:00:49 +0100

    Add home view template with basic HTML structure

---
+++ private/views/home.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>assets/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>assets/all.min.css" >
+
+    <title>Home</title>
+</head>
+<body>
+    <h1 class="text-danger">
+       Home Page
+    </h1>
+
+</body>
+</html>
