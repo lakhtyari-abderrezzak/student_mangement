920f4edb410fb024eaf0ddba1cfd801392a09285
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-20 21:40:13 +0100

    Refactor home view to include header and footer, update content

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>assets/bootstrap.min.css" >
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>assets/all.min.css" >
+<?php echo $this->view('includes/header'); ?>
 
-    <title>Home</title>
-</head>
-<body>
-    <h1 class="text-danger">
-       Home Page
-    </h1>
+<h1>Hello guys</h1>
 
-</body>
-</html>
+<?php echo $this->view('includes/footer'); ?>
