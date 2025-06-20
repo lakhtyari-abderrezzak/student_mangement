6ae7474795f5ced417cf914c5e868937fc407311
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-20 21:39:12 +0100

    Add header view template for the homepage

---
+++ private/views/includes/header.view.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>assets/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>assets/all.min.css" >
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>assets/main.css" >
+
+    <title>Home</title>
+</head>
+<div class="container">
