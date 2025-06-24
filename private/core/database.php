ebd890c90df3fc2fa215ad8c2614cf08d6110cb3
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-24 11:54:09 +0100

    Add Database class with connection and query methods

---
+++ private/core/database.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Database{
+
+    private function connect(){
+         $dsn =  DB_DSN;
+         try {
+            $conn = new PDO($dsn, DB_USER, DB_PASS);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+            } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            exit;
+            }
+    }
+
+    public function query($query, $params = [], $fetchMode = "object"){
+        $con = $this->connect();
+        $stmt = $con->prepare($query);
+        
+        if($stmt){
+            $check = $stmt->execute($params);
+            if($check){
+                if($fetchMode !== 'object'){
+                    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                } else{
+                    return $stmt->fetchAll(PDO::FETCH_OBJ);
+                } 
+            }
+        } 
+        return false;
+    }
+
+}
