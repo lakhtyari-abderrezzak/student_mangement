406dce5d254054667df8370e27712cdaf5333b01
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-24 12:36:51 +0100

    Implement autoloading for model classes to streamline class loading process

--- private/core/autoload.php
+++ private/core/autoload.php
@@ -5,3 +5,14 @@ require_once __DIR__ . '/App.php';
 require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/Controller.php';
 require_once __DIR__ . '/Model.php';
+
+
+spl_autoload_register(function($classname){
+    $file = '../private/models/' . $classname . '.php';
+    if(file_exists($file)){
+        require_once $file;
+    }else{
+        echo "Model file for $classname not found.";
+        exit;
+    }
+});
