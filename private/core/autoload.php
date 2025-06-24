31a2119a97451955b21498d6d8f86f737092b93a
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-24 11:59:05 +0100

    Fix case sensitivity in autoload file: update require statements for App and Database classes

--- private/core/autoload.php
+++ private/core/autoload.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/app.php';
-require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/App.php';
+require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/Controller.php';
+require_once __DIR__ . '/Model.php';
