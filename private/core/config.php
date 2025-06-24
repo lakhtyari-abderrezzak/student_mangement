2dac0f0fcf73e6b5c74fd1dc73f57fe82f1ac62d
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-24 11:56:39 +0100

    Refactor database configuration: add constants for DB connection details

--- private/core/config.php
+++ private/core/config.php
@@ -1,3 +1,11 @@
 <?php
 
 define("ROOT", "http://localhost/student_management/public/");
+
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "student_db");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_CHARSET", "utf8mb4");
+define("DB_DSN", "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET);
