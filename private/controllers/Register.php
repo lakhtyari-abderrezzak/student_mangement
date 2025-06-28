5354d18c4543c91157b929ed1ec7c900ad88c8fa
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-28 11:30:53 +0100

    Implement user registration logic with validation and error handling

--- private/controllers/Register.php
+++ private/controllers/Register.php
@@ -4,6 +4,25 @@ use \core\Controller;
 
 class Register extends Controller{
     public function index(){
-        echo $this->view('register');
+        if(isset($_POST)){
+            $user = new User();
+
+            if($user->validate($_POST)){
+                
+                
+                $this->redirect('login');
+                
+            }else{
+
+                $errors = $user->errors;
+            }
+
+
+        }
+        echo $this->view('register', [
+            'title' => 'Register',
+            'errors' => $errors ?? []
+        ]);
+       
     }
 } 
