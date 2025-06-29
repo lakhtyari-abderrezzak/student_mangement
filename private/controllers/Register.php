1f36edbbf4aacba19cec597ea3bc886bd39a1b9b
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-29 12:05:03 +0100

    Refactor registration logic to improve POST request handling and streamline user data insertion

--- private/controllers/Register.php
+++ private/controllers/Register.php
@@ -4,12 +4,23 @@ use \core\Controller;
 
 class Register extends Controller{
     public function index(){
-        if(isset($_POST)){
+        if(count($_POST) > 0){
             $user = new User();
 
             if($user->validate($_POST)){
                 
-                
+
+                $arr = [
+                    'first_name' => getValue('first_name'),
+                    'last_name' => getValue('last_name'),
+                    'email' => getValue('email'),
+                    'rank' => getValue('rank'),
+                    'gender' => getValue('gender'),
+                    'password' => password_hash(getValue('password'), PASSWORD_BCRYPT),
+                    'date' => date('Y-m-d H:i:s'),
+                ];
+
+                $user->insert($arr);
                 $this->redirect('login');
                 
             }else{
