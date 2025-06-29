ba39d5935376c5243c22b6c9a9f09a88badd6dd8
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-29 12:05:37 +0100

    Add missing functions.php require statement in autoload file

--- private/core/autoload.php
+++ private/core/autoload.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/App.php';
 require_once __DIR__ . '/Database.php';
+require_once __DIR__ . '/functions.php';
 require_once __DIR__ . '/Controller.php';
 require_once __DIR__ . '/Model.php';
 
