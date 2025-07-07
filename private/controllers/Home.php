79940bf62bcc13486b252e888dfa9885e9b523dd
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-07-07 12:43:19 +0100

    Refactor Home controller: improve code formatting and enhance readability of the index method

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -3,6 +3,10 @@ use core\Controller;
 class Home extends Controller
 {
     public function index(){
+
+        if(!Auth::check()){
+            $this->redirect('login');
+        }
         echo $this->view('home');
     }
    
