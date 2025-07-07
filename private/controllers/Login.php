aab4c75bf7aeefb5108f47aef291e7dcb9f51976
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-07-07 12:43:52 +0100

    Refactor Login controller: implement user authentication logic and enhance error handling in the index method

--- private/controllers/Login.php
+++ private/controllers/Login.php
@@ -6,7 +6,29 @@ class Login extends Controller
 
     public function index()
     {
-        $this->view('login');
+        
+        if (count($_POST) > 0) {
+            $errors = [];
+            $user = new User();
+
+            if ($user->where('email', getValue('email'))) {
+                $user = $user->where('email', getValue('email'))[0];
+
+                if(password_verify($_POST['password'], $user->password)){
+                    Auth::attempt($user);
+                    $this->redirect('home');
+                }
+            } else {
+                $errors['email'] = "Email or password is incorrect";
+            } 
+
+
+        }
+        echo $this->view('login', [
+            'title' => 'login',
+            'errors' => $errors ?? []
+        ]);
+
     }
 
 }
