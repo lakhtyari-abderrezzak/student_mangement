ec883c2c39a62bbdaa95fe908fe86931813aa3f0
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-07-12 02:41:48 +0100

    Add dynamic user property access: implement __callStatic method for flexible user attribute retrieval

--- private/models/Auth.php
+++ private/models/Auth.php
@@ -15,4 +15,19 @@ Class Auth {
         return isset($_SESSION['user']);
     }
 
+    public static function user() {
+        return isset($_SESSION['user']) ? $_SESSION['user']->first_name : null;
+    }
+
+    public static function __callStatic($methods, $props){
+        if (isset($_SESSION['user'])) {
+            $props = strtolower(str_replace('get', '', $methods));
+            if (isset($_SESSION['user']->$props)) {
+                return $_SESSION['user']->$props;
+            } else {
+                return null;
+            }
+        }
+    }
+
 }
