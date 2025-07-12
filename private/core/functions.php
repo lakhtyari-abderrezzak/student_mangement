9b3de66997dfd0e6334ec296791456dfdb79eb71
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-07-12 02:40:04 +0100

    Refactor functions: improve code readability and maintainability

--- private/core/functions.php
+++ private/core/functions.php
@@ -1,17 +1,31 @@
-<?php 
-function getValue($key){
+<?php
+function getValue($key)
+{
 
-    if(isset($_POST[$key])){
+    if (isset($_POST[$key])) {
         return htmlspecialchars(trim($_POST[$key]));
-    } 
+    }
     return '';
 }
-function getSelected($key, $value){
-    if(isset($_POST[$key]) && $_POST[$key] == $value){
+function getSelected($key, $value)
+{
+    if (isset($_POST[$key]) && $_POST[$key] == $value) {
         return 'selected';
     }
     return '';
 }
-function esc($value){
+function esc($value)
+{
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+}
+
+function randomId($length)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
