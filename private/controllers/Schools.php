bb11fe22051a11b4dbd547a5d993a54df2e1b80d
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-07-20 11:42:14 +0100

    Refactor Schools controller: add authentication checks for add, edit, and delete methods

--- private/controllers/Schools.php
+++ private/controllers/Schools.php
@@ -17,12 +17,15 @@ class Schools extends Controller
     }
     public function add()
     {
+        if (!Auth::check()) {
+            return $this->redirect('login');
+        }
         $errors = [];
 
         if (count($_POST) > 0) {
 
             $school = new School();
-      
+
             if (!$school->validate($_POST)) {
 
 
@@ -43,5 +46,53 @@ class Schools extends Controller
         echo $this->view('schools.add', [
             'errors' => $errors,
         ]);
+    }
+    public function edit($id = null)
+    {
+        if (!Auth::check()) {
+            return $this->redirect('login');
+        }
+        $errors = [];
+
+        $school = new School();
+
+        $row = $school->findById($id)[0] ;
+        if (!$row) {
+            $this->redirect('schools');
+        }
+
+        if (count($_POST) > 0) {
+            
+
+            if (!$school->validate($_POST)) {
+
+                $arr = [
+                    'school' => getValue('school'),
+                    'date' => date('Y-m-d H:i:s'),
+                ];
+
+                $school->update($id,  $arr);
+                $this->redirect('schools');
+
+            } else {
+                $errors = $school->errors;
+            }
+
+
+        }
+        echo $this->view('schools.edit', [
+            'errors' => $errors,
+            'row' => $row,
+        ]);
+    }
+    public function delete($id)
+    {
+        if(!Auth::check()) {
+            return $this->redirect('login');
+        }
+        $school = new School();
+        $school->delete($id);
+        $this->redirect('schools');
+        exit;
     }
 }
