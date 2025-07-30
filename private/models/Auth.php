97dbee686eb4eb7f26dcc6d0722ba573febbabcd
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-07-31 00:10:28 +0100

    Refactor Auth class: improve code formatting and enhance __callStatic method return value

--- private/models/Auth.php
+++ private/models/Auth.php
@@ -1,33 +1,62 @@
-<?php 
+<?php
 
 
-Class Auth {
+class Auth
+{
 
-    public static function attempt($user) {
-            $_SESSION['user'] = $user;
+    public static function attempt($user)
+    {
+        $_SESSION['user'] = $user;
     }
 
-    public static function logout() {
+    public static function logout()
+    {
         unset($_SESSION['user']);
     }
 
-    public static function check() {
+    public static function check()
+    {
         return isset($_SESSION['user']);
     }
 
-    public static function user() {
+    public static function user()
+    {
         return isset($_SESSION['user']) ? $_SESSION['user']->first_name : null;
     }
 
-    public static function __callStatic($methods, $props){
+    public static function __callStatic($methods, $props)
+    {
         if (isset($_SESSION['user'])) {
             $props = strtolower(str_replace('get', '', $methods));
             if (isset($_SESSION['user']->$props)) {
                 return $_SESSION['user']->$props;
             } else {
-                return null;
+                return 'Unknown';
             }
         }
     }
+    public static function switchSchool($id)
+    {
+        if (isset($_SESSION['user']) && $_SESSION['user']->rank === 'super-admin') {
+            $user = new User();
+            $school = new School();
 
+            $row = $school->where('id', $id);
+            if ($row) 
+            {
+
+                $row = $row[0];
+
+                $arr['school_id'] = $row->school_id;
+               $user->update($_SESSION['user']->id, $arr);
+                $_SESSION['user']->school_id = $row->school_id;
+                $_SESSION['user']->school_name = $row->school;
+
+                return true;
+                 
+                
+            }
+        }
+        return false;
+    }
 }
